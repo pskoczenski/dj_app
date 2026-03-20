3e87ba7078a27939b47269831a0093bf236275b1
Peter Skoczenski <pskoczenski@gmail.com>
2026-03-19 23:11:14 -0400

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Fraunces, Libre_Franklin, JetBrains_Mono } from "next/font/google";
+import { Fraunces, Libre_Franklin, JetBrains_Mono, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const fraunces = Fraunces({
   variable: "--font-fraunces",
@@ -36,7 +39,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`dark ${fraunces.variable} ${libreFranklin.variable} ${jetbrainsMono.variable} h-full antialiased`}
+      className={cn("dark", "h-full", "antialiased", fraunces.variable, libreFranklin.variable, jetbrainsMono.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
